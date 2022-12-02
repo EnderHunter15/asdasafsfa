420eab6ad4473e6221ac63364235c8e8cf2e824c
Dinu <coatu_dinu@yahoo.com>
2022-12-02 17:10:33 +0200

    am dat printf la cv

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 import products from "./products.json" assert { type: "json" };
-<<<<<<< HEAD
-console.log("123");
-=======
-
-products.map((product) => console.log(product.title));
->>>>>>> 26a330f03b5db45e13d2bd7c39ddd03d1f2f6802
+for(let i=0;i<3;i++)
+console.log("alex e boolangiu");
