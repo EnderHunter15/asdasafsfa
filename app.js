2ef3295cfdb0b923eed8ca91540676bdd27290ef
Dinu <coatu_dinu@yahoo.com>
2022-12-02 22:14:01 +0200

    products grid

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-import products from "./products.json" assert { type: "json" };
+import products from "./products.json" assert {type: "json"};
 
-products.map(product => console.log(product.title))
+const content = document.querySelector(".content");
+
+function createItem(product) {
+  return `<div class="product">
+  <img src="${product.imageUrl}" >
+  <h1 class="title">${product.title}</h1>
+  <p class="price">$${product.price} Ron</p>
+  </div>`;
+}
+
+products.map((item) => {
+  content.innerHTML = content.innerHTML + createItem(item);
+});
